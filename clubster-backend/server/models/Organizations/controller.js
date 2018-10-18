8f5e282f3d7e38662f2667a956e7567b13393851
Maryam Majidi <mmajidi@ucsc.edu>
2018-10-18 12:18:01 -0700

    post request for clubs done

--- clubster-backend/server/models/Organizations/controller.js
+++ clubster-backend/server/models/Organizations/controller.js
@@ -8,7 +8,7 @@ const Organizations = require('./model');
 
 
 exports.getAllClubs = (req, res) => {
-	// Code to display all the clubs in out Mongo Collection
+	// Code to display all the clubs in our Mongo Collection
 	Organizations.find().then((organizations) => {
 		if (!organizations)
 		{
@@ -20,4 +20,51 @@ exports.getAllClubs = (req, res) => {
 		}
 	});
 
-}
+};
+
+exports.addOrg = (req, res) => {
+	// Code to add a new organization to the Mongo Collection
+	
+	// Destruct req body
+	const{President, VicePresident, Treasurer, Secretary, EventsChair, CommunicationsChair, WebMaster, description, social} = req.body;
+	// If user supplied these fields
+	if(President && VicePresident && Treasurer && Secretary && description) {
+		// Check if organization with these key-value pairs already exists
+		Organizations.findOne( { $and: [
+			{President: President},
+			{VicePresident: VicePresident},
+			{Treasurer: Treasurer},
+			{Secretary: Secretary},
+			{description: description}]
+		}).exec(function(err, organization){
+			if(organization){
+				return req.status(400).json({'Error': 'Organization already exists'});
+			}
+			else{
+				// Make new organization and save into the Organizations Collection
+				let newOrg = {
+					President: President,
+					VicePresident: VicePresident,
+					Treasurer: Treasurer,
+					Secretary: Secretary,
+					EventsChair: EventsChair,
+					CommunicationsChair: CommunicationsChair,
+					WebMaster: WebMaster,
+					description: description
+				};
+ 				newOrg.save().then(organization => res.json(organization).catch(err => console.log(err))); // Push the new user onto the db if successful, else display error
+
+				return req.status(201).json(newOrg);
+			}
+		})
+
+
+		return res.status(201).json({'Success': 'Success'});
+	}
+	else {
+		console.log(President);
+		return res.status(201).json({'Error': 'Error'});
+	}
+
+};
+
--- clubster-backend/server/models/Organizations/routes.js
+++ clubster-backend/server/models/Organizations/routes.js
@@ -10,4 +10,8 @@ router.get('/organization/all', (req, res) => {
 	controller.getAllClubs(req, res);
 });
 
+router.post('/organization/new', (req, res) => {
+	controller.addOrg(req, res);
+});
+
 module.exports = router;
