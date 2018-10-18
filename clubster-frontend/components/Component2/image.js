eea799230b0c50fed26bdf04c82138cd5366df4e
moelattma <moelattma@gmail.com>
2018-10-18 12:45:40 -0700

    Restructured and changed some UI

--- clubster-frontend/components/Component2/image.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react'
-import { Image } from 'react-native'
-
-const ImagesExample = () => (
-   <Image source = {require('/Users/olusyarbareach/Desktop/cmps-115/frontend/club.jpg')} />
-)
-export default ImagesExample
