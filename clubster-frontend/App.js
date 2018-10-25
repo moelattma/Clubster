6540551ff71c62d2d3fe428efb5fa9d067a29d05
Olusyar Bareach <olusyarbareach@eduroam-169-233-225-118.ucsc.edu>
2018-10-25 16:55:52 -0700

    profile UI

--- clubster-frontend/App.js
+++ clubster-frontend/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
-import { LoginNavigator } from './components/router'
+import { HomeNavigator } from './components/router'
+
+//import Profile from './components/Profile'
 
 export default class Clubster extends Component {
   render() {
     return (
-      <LoginNavigator/>
+      <HomeNavigator/>
     );
   }
 }
