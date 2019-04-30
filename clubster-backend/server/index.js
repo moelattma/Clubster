c615bf4429c75665d7acc25cd7f844930119186a
moelattma <moelattma@gmail.com>
2019-04-30 00:18:59 -0700

    added cors

--- clubster-backend/server/index.js
+++ clubster-backend/server/index.js
@@ -28,6 +28,7 @@ const app = express();
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit:50000}));
 app.use(morgan('dev')); //debugging for HTTP requests
+app.use(require('cors'));
 
 // Passport middleware
 app.use(passport.initialize());
--- clubster-backend/server/models/Users/routes.js
+++ clubster-backend/server/models/Users/routes.js
@@ -6,12 +6,13 @@
 const router = require('express').Router();
 const controller = require('./controller');
 const passport = require('passport');
+const cors = require('cors')
 
 router.post('/register', (req, res) => {
 	controller.createUser(req, res);
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', cors(), (req, res) => {
 	controller.findUser(req, res);
 });
 
