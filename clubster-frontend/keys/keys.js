68a2f9b41d831c6dc6bbd63760b013e89547b6f1
Kirby Lam <klam18@ucsc.edu>
2019-06-08 14:31:04 -0700

    event edit and UI changes

---
+++ clubster-frontend/keys/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    secret: 's904374jsajkfehifuye343948!fhdiufhnewklfjewi',
+    databaseUsername: 'clubster123',
+    databasePassword: 'Clubster123!',
+    accessKeyId: 'AKIAJ5RZ44QFXTJFIBNA',
+    secretAccessKey: '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',
+    awsLink: 'https://s3.amazonaws.com/clubster-123/'
+}
+
